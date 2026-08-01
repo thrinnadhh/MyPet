8dede8fb8df7c3aae928761f1b54e40a23f05a0e
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-08-01 12:28:59 +0530

    refactor(m7): delegate content scheduler locking to shared factory

--- backend/content-service/src/main/kotlin/com/pawsnearme/contentservice/config/ShedLockConfig.kt
+++ backend/content-service/src/main/kotlin/com/pawsnearme/contentservice/config/ShedLockConfig.kt
@@ -1,23 +1,19 @@
 package com.pawsnearme.contentservice.config
 
+import com.pawsnearme.common.scheduling.SchedulerLockProviderFactory
 import net.javacrumbs.shedlock.core.LockProvider
-import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.core.JdbcTemplate
 import javax.sql.DataSource
 
 @Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "PT30M")
 class ShedLockConfig {
     @Bean
-    fun lockProvider(dataSource: DataSource): LockProvider =
-        JdbcTemplateLockProvider(
-            JdbcTemplateLockProvider.Configuration.builder()
-                .withJdbcTemplate(JdbcTemplate(dataSource))
-                .withTableName("content.shedlock")
-                .usingDbTime()
-                .build()
-        )
+    fun lockProvider(
+        dataSource: DataSource,
+        @Value("\${mypet.scheduling.lock-table:content.shedlock}") tableName: String
+    ): LockProvider = SchedulerLockProviderFactory.create(dataSource, tableName)
 }
