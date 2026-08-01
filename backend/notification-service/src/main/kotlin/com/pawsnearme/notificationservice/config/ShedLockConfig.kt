73337c62abc332fc07335346205623d4d6ebe4d9
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-08-01 12:32:59 +0530

    feat(m7): install notification scheduler role infrastructure

--- backend/notification-service/src/main/kotlin/com/pawsnearme/notificationservice/config/ShedLockConfig.kt
+++ backend/notification-service/src/main/kotlin/com/pawsnearme/notificationservice/config/ShedLockConfig.kt
@@ -1,17 +1,22 @@
 package com.pawsnearme.notificationservice.config
 
 import com.pawsnearme.common.scheduling.SchedulerLockProviderFactory
+import com.pawsnearme.common.scheduling.SchedulerRuntimeInfrastructureConfiguration
+import com.pawsnearme.common.scheduling.WorkerScheduler
 import net.javacrumbs.shedlock.core.LockProvider
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import javax.sql.DataSource
 
 @Configuration
+@Import(SchedulerRuntimeInfrastructureConfiguration::class)
 @EnableSchedulerLock(defaultLockAtMostFor = "PT30M")
 class ShedLockConfig {
     @Bean
+    @WorkerScheduler
     fun lockProvider(
         dataSource: DataSource,
         @Value("\${mypet.scheduling.lock-table:notifications.shedlock}") tableName: String
