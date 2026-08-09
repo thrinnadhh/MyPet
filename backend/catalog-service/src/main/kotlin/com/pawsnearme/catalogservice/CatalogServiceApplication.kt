eb563d493b67b12d616fe362dbe9512b41eb1858
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-08-09 10:13:31 +0530

    fix: keep catalog application bootstrap web-test friendly

--- backend/catalog-service/src/main/kotlin/com/pawsnearme/catalogservice/CatalogServiceApplication.kt
+++ backend/catalog-service/src/main/kotlin/com/pawsnearme/catalogservice/CatalogServiceApplication.kt
@@ -1,17 +1,11 @@
 package com.pawsnearme.catalogservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-@EntityScan(basePackages = ["com.pawsnearme.catalogservice", "com.pawsnearme.common.outbox"])
-@EnableJpaRepositories(basePackages = ["com.pawsnearme.catalogservice", "com.pawsnearme.common.outbox"])
-@ComponentScan(basePackages = ["com.pawsnearme.catalogservice", "com.pawsnearme.common.outbox"])
 class CatalogServiceApplication
 
 fun main(args: Array<String>) {
